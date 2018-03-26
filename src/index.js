78b0d4b7148e5b3ec1b3fa285805659f09551c89
871246995 <871246995@qq.com>
2018-03-26 16:13:09 +0800

    #output management

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import _ from 'lodash';
-import  './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+// import  './style.css';
+// import Icon from './icon.png';
+// import Data from './data.xml';
+import printMe from './print.js';
   function component() {
     var element = document.createElement('div');
-      element.innerHTML = _.join(['Hello', 'webpack--------'], ' ');
-      element.classList.add('hello');
+    var btn = document.createElement('button');
+    element.innerHTML = _.join(['Hello', 'webpack--------'], ' ');
+    element.classList.add('hello');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
-  // Add the image to our existing div.
-   var myIcon = new Image();
-  //  myIcon.src = Icon;
+  // // Add the image to our existing div.
+  //  var myIcon = new Image();
+  // //  myIcon.src = Icon;
 
-   element.appendChild(myIcon);
-    console.log(Data, 'data-----');
-    console.log(Icon,'Icon-----');
-    alert(Icon)
-   alert(Data.body)
-   console.log('223333333333');
+  //  element.appendChild(myIcon);
+  //   console.log(Data, 'data-----');
+  //   console.log(Icon,'Icon-----');
+  //   alert(Icon)
+  //  alert(Data.body)
+  //  console.log('223333333333');
    
    
     return element;
