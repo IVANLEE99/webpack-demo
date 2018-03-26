35aeb8d30240906dbc1f961a78e67ee37b00168c
871246995 <871246995@qq.com>
2018-03-26 17:38:57 +0800

    hmr

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import _ from 'lodash';
-// import  './style.css';
-// import Icon from './icon.png';
-// import Data from './data.xml';
+import  './style.css';
 import printMe from './print.js';
   function component() {
     var element = document.createElement('div');
     var btn = document.createElement('button');
-    element.innerHTML = _.join(['Hello', 'webpa222222ck---2-23watch----'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpa222222ck---2-223watch----'], ' ');
     element.classList.add('hello');
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
@@ -26,5 +24,16 @@ import printMe from './print.js';
    
     return element;
   }
+let element = component();
+document.body.appendChild(element);
 
-document.body.appendChild(component());
+if (module.hot) {
+  module.hot.accept('./print.js',function () {  
+    console.log('acception the updated printMe module!');
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
+    printMe();
+    
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
+  mode:'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
+    // print: './src/print.js'
   },
   devtool: 'inline-source-map',
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new ManifestPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
-    }
-  };
+  },
+  devServer:{
+    contentBase:'./dist',
+    hot:true
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:[
+          'style-loader',
+          'css-loader'
+        ]
+      },{
+        test: /\.(png|svg|jpg|gif)$/,
+        use:[
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
