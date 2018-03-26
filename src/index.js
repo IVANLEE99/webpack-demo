0804c5db5be8b4a4ba08d03dad1745b7aeb31c99
871246995 <871246995@qq.com>
2018-03-26 17:01:53 +0800

    dev-server-middleware

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import printMe from './print.js';
   function component() {
     var element = document.createElement('div');
     var btn = document.createElement('button');
-    element.innerHTML = _.join(['Hello', 'webpack---2-23watch----'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpa222222ck---2-23watch----'], ' ');
     element.classList.add('hello');
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
